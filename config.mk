5378ffa2dbd3abfced0e10efdb580ba7dec75902
Dmitry <dmitriyhetman@gmail.com>
2017-03-10 12:18:55 +0000

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # wld: config.mk
 
 CC                  = gcc
-CFLAGS              = -O2 -Wall -Wextra -Wno-unused-parametr -Wno-unused-const-variable -pipe
+CFLAGS              = -O1 -Wall -Wno-unused-parametr -Wno-unused-const-variable -pipe
 
 ENABLE_DEBUG        = 1
 ENABLE_STATIC       = 1
