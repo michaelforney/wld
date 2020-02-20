930aaf9388b665ae728b8ed75771896d1fb43245
nia <nia@netbsd.org>
2020-02-20 13:13:04 +0000

    Replace ifdef NetBSD with ftruncate fallback

--- wayland-shm.c
+++ wayland-shm.c
@@ -177,11 +177,7 @@ context_create_buffer(struct wld_context *base,
 
 	unlink(name);
 
-#ifndef __NetBSD__
-	if (posix_fallocate(fd, 0, size) != 0)
-#else
-	if (ftruncate(fd, size) != 0)
-#endif
+	if (posix_fallocate(fd, 0, size) != 0 && ftruncate(fd, size) != 0)
 		goto error2;
 
 	if (!(pool = wl_shm_create_pool(context->wl, fd, size)))
